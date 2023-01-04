2db1b90b598981d90793aa04ba8a9b01a70afcb1
yurii_yanchuk <yurii_yanchuk@epam.com>
2023-01-04 21:02:56 +0200

    add tests

--- src/test/java/core/LogTest.java
+++ src/test/java/core/LogTest.java
@@ -1,13 +1,18 @@
 package core;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import org.awaitility.Awaitility;
 import org.awaitility.core.ConditionFactory;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -17,6 +22,8 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Supplier;
 import java.util.logging.Level;
 
+import static com.codeborne.selenide.Selenide.$x;
+
 public class LogTest {
 
     private final Supplier<ConditionFactory> WAITER = () -> Awaitility.given()
@@ -24,7 +31,7 @@ public class LogTest {
             .pollInterval(3, TimeUnit.SECONDS)
             .await()
             .dontCatchUncaughtExceptions()
-            .atMost(10, TimeUnit.SECONDS);
+            .atMost(15, TimeUnit.SECONDS);
 
     private boolean waitLogs(String expectedMessage) {
         WebDriver driver = WebDriverRunner.getWebDriver();
@@ -33,10 +40,17 @@ public class LogTest {
 
         WAITER.get().until(() -> {
             LogEntries logEntries = driver.manage().logs().get(LogType.BROWSER);
+            printLogs(logEntries);
             isLogContains.set(logEntries.getAll().stream().anyMatch(x -> x.getMessage().contains(expectedMessage)));
             return isLogContains.get();
         });
         return isLogContains.get();
+    }
+
+    private void printLogs(LogEntries logEntries) {
+        for (LogEntry entry : logEntries) {
+            System.out.println(entry.getMessage());
+        }
     }
 
     @BeforeAll
@@ -55,5 +69,29 @@ public class LogTest {
         Configuration.browserCapabilities = capabilities;
         Configuration.timeout = 10000;
         Configuration.pageLoadTimeout = 10000;
+    }
+
+    @Test
+    public void checkLogsWithDelay() {
+
+        Selenide.open("https://www.gismeteo.com/");
+        boolean isLogExist = waitLogs("ERR_FILE_NOT_FOUND");
+        Assertions.assertTrue(isLogExist, "https://www.gismeteo.com/ - The resource https://static.gismeteo.st/assets/bg-header/n.jpg was preloaded using link preload but not used within a few seconds from the window's load event. Please make sure it has an appropriate `as` value and it is preloaded intentionally.");
+    }
+
+    @Test
+    public void logsErrorTest() {
+
+        String errorMessage = "https://www.gismeteo.com/ - The resource https://static.gismeteo.st/assets/bg-header/n.jpg was preloaded using link preload but not used within a few seconds from the window's load event. Please make sure it has an appropriate `as` value and it is preloaded intentionally.";
+
+        Selenide.open("https://www.gismeteo.com/");
+        Configuration.browserSize = "1920x1080";
+        $x("//a[@href='/weather-london-744/']");
+        $x("//h1[text()='London Weather Forecast']").should(Condition.visible);
+
+        boolean isContainsLogs = waitLogs(errorMessage);
+
+        Assertions.assertTrue(isContainsLogs, "Error message " + errorMessage + " is not found");
+
     }
 }
